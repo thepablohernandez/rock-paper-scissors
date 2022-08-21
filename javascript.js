692d24a592192a0192e7d3c146c3527f34e1ab63
Pablo <pablo-hernandez@live.com.ar>
2022-08-21 18:28:03 -0300

    Add the first function and assing its result
    add the first function of the .js file, which generates a random number
    and assing its value to a variable called 'computer selection'
    
    by now, the function only assings the result 'Rock' to a random number...
    ... lower than 1/3
    
    having checked that it works ok, git commit to save and go on

---
+++ javascript.js
@@ -0,0 +1,26 @@
+/* For the first section of the file I will work on one or more functions
+to generate a random choice for the computer (between rock, paper or scissors) and save it
+to later on use it and compare against users choice*/
+
+// generate a function that randomly selects rock, paper or scissors
+// to do so, I generate random numbers
+
+function getComputerChoice () {
+
+    // I assign the result with the same probabilty, dividing the math.random in 3:
+    // at 1/3 and 2/3
+    
+    let choice = Math.random();
+    if (choice <= 1/3) {
+        console.log(choice);
+        return 'Rock'
+    }
+    else {
+        return choice;
+    }
+}
+
+// ... then, save the result of the previous function in the variable ComputerSelection
+
+let ComputerSelection = getComputerChoice ();
+console.log(ComputerSelection); // this is just to test the function and its assingment
